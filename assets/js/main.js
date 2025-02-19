7582bb9477db8eaaaaec3bbc57cd72969d1c9618
nahian28 <nahiansylhet@gmail.com>
2025-02-19 09:41:02 +0600

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,60 @@
-$(document).ready(function () {
+jQuery(document).ready(function ($) {
     $('.faq-question').click(function () {
-        var answer = $(this).next('.faq-answer');
-        var icon = $(this).find('.faq-icon');
-        
-        $('.faq-answer').not(answer).slideUp();
-        $('.faq-icon').not(icon).removeClass('fa-minus').addClass('fa-plus').css('transform', 'rotate(0deg)');
-        
-        answer.slideToggle();
-        if (icon.hasClass('fa-plus')) {
-            icon.removeClass('fa-plus').addClass('fa-minus').css('transform', 'rotate(180deg)');
-        } else {
-            icon.removeClass('fa-minus').addClass('fa-plus').css('transform', 'rotate(0deg)');
-        }
+        var $this = $(this);
+        var $answer = $this.next('.faq-answer');
+        var $icon = $this.find('.faq-icon');
+
+        $('.faq-answer').not($answer).stop(true, true).slideUp();
+        $('.faq-icon').not($icon).removeClass('fa-minus').addClass('fa-plus').css('transform', 'rotate(0deg)');
+
+        $answer.stop(true, true).slideToggle();
+        $icon.toggleClass('fa-plus fa-minus').css('transform', $icon.hasClass('fa-plus') ? 'rotate(0deg)' : 'rotate(180deg)');
+    });
+
+
+    $('.image-faq-question').click(function () {
+        var $this = $(this);
+        var $answer = $this.next('.image-faq-answer');
+        var $icon = $this.find('.image-faq-icon');
+
+        $('.image-faq-answer').not($answer).stop(true, true).slideUp();
+        $('.image-faq-icon').not($icon).removeClass('fa-minus').addClass('fa-plus').css('transform', 'rotate(0deg)');
+
+        $answer.stop(true, true).slideToggle();
+        $icon.toggleClass('fa-plus fa-minus').css('transform', $icon.hasClass('fa-plus') ? 'rotate(0deg)' : 'rotate(180deg)');
     });
 });
+
+(function ($) {
+    "use strict";
+
+    var $projects = $(".projects");
+
+    if ($projects.length) {
+        $projects.isotope({
+            itemSelector: ".item",
+            layoutMode: "fitRows"
+        });
+
+        $("ul.filters").on("click", "li", function (e) {
+            e.preventDefault();
+            var filter = $(this).attr("data-filter");
+
+            $("ul.filters > li").removeClass("active");
+            $(this).addClass("active");
+
+            $projects.isotope({ filter: filter });
+        });
+    }
+
+    $(".project").hover(
+        function () {
+            $(this).find(".project-overlay").stop(true, true).css({ top: "-100%" });
+            $(this).find(".project-hover").stop(true, true).css({ top: "0" });
+        },
+        function () {
+            $(this).find(".project-overlay").stop(true, true).css({ top: "0" });
+            $(this).find(".project-hover").stop(true, true).css({ top: "100%" });
+        }
+    );
+})(jQuery);
--- header.php
+++ header.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php wp_title('|', true, 'right'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">=
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
