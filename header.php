84bec406eeee5bbc1d88a526205ce07023e05bb8
nahian28 <nahiansylhet@gmail.com>
2025-03-10 00:30:49 +0600

    Updates

--- header.php
+++ header.php
@@ -48,6 +48,24 @@
                             ));
                         ?>
                     </div>
+                    
+                </div>
+                <div class="col-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <?php
+                            wp_nav_menu(array(
+                                'theme_location' => 'primary',
+                            ));
+                        ?>
+    </div>
+  </div>
+</nav>
                 </div>
             </div>
         </div>
